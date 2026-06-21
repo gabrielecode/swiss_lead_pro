16742c48f03158a6ee02a0842fc3eb5cc041681a
gabrielecode <gestsocialgabriele@gmail.com>
2026-06-21 16:00:29 +0200

    Add GitHub Pages configuration and build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/swiss_lead_pro/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
